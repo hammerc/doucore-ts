2b1e967199c9b3b25f2267bc86373185c46c5c13
hammerc <hammerc@163.com>
2020-06-08 11:27:19 +0800

    对象池调整。

--- core/src/utils/Recyclable.ts
+++ core/src/utils/Recyclable.ts
@@ -26,7 +26,34 @@ namespace dou {
     /**
      * 对象池配置
      */
-    export function deployPool(targetClass: { new(): any }, maxCount: number): void {
-        (<any>targetClass.prototype.constructor).__cacheMaxCount = maxCount;
+    export function deployPool(creator: Creator<any> & { __pool?: ObjectPool<any> }, maxCount: number): void {
+        (<any>creator.prototype.constructor).__cacheMaxCount = maxCount;
+    }
+
+    /**
+     * 获取对象池中的对象数量
+     */
+    export function getPoolSize(creator: Creator<any> & { __pool?: ObjectPool<any> }): number {
+        let pool: ObjectPool<any>;
+        if (creator.hasOwnProperty("__pool")) {
+            pool = creator.__pool;
+        }
+        if (pool) {
+            return pool.size;
+        }
+        return 0;
+    }
+
+    /**
+     * 清空对象池
+     */
+    export function clearPool(creator: Creator<any> & { __pool?: ObjectPool<any> }): void {
+        let pool: ObjectPool<any>;
+        if (creator.hasOwnProperty("__pool")) {
+            pool = creator.__pool;
+        }
+        if (pool) {
+            pool.clear();
+        }
     }
 }
